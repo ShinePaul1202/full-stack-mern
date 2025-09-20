a4d2fd987ed5ef3df6aec6565f21f89dc7780b39
ShinePaul1202 <24mp2316@rit.ac.in>
2025-09-20 09:16:57 +0530

    js

---
+++ array.js
@@ -0,0 +1,27 @@
+let mixedArray = [1, "two", 3, "four", 5];
+console.log(mixedArray);
+
+const nestedArray = [[1, 2], [3, 4]];
+for (Array of nestedArray){
+    console.log(Array);
+}
+console.log(nestedArray[0]);
+console.log(nestedArray[0][1]);//2
+console.log(nestedArray[1][0]);//3
+
+let names = ["Alice", "Bob", "Charlie"];
+names[0]="Bob"
+console.log(names);
+names.push("David")
+console.log(names);
+names.pop()
+console.log(names);
+console.log(names.length);
+
+//empty array
+let newArray = []
+let newArray1 = new Array()
+
+//array Methods
+const  fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.forEach
